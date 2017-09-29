a22a6b3faf8f79180930971c401567d476cf5cde
Krzysztof Kąkol <krix38@gmail.com>
2017-09-29 20:04:11 +0200

    added action exception

---
+++ src/main/java/com/github/krix38/flowly/exception/ActionExecutionException.java
@@ -0,0 +1,17 @@
+package com.github.krix38.flowly.exception;
+
+/**
+ * Created by krix on 29.09.2017.
+ */
+public class ActionExecutionException extends Exception {
+    public ActionExecutionException() {
+    }
+
+    public ActionExecutionException(String message) {
+        super(message);
+    }
+
+    public ActionExecutionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
