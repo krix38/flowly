c099c0d96a60898e71a1cafe6ba34d3933fd634d
Krzysztof Kąkol <krix38@gmail.com>
2018-02-21 16:33:31 +0100

    added support for jaxb models

--- src/main/java/com/github/krix38/flowly/model/AbstractModel.java
+++ src/main/java/com/github/krix38/flowly/model/AbstractModel.java
@@ -1,13 +1,16 @@
 package com.github.krix38.flowly.model;
 
+import javax.xml.bind.annotation.XmlTransient;
 import java.util.Optional;
 
 /**
  * Created by krix on 29.09.2017.
  */
 
+@XmlTransient
 public abstract class AbstractModel {
 
+    @XmlTransient
     private Optional<FailureInformation> failureInformation = Optional.empty();
 
     public FailureInformation getFailureInformation() {
--- src/main/java/com/github/krix38/flowly/model/FailureInformation.java
+++ src/main/java/com/github/krix38/flowly/model/FailureInformation.java
@@ -1,11 +1,18 @@
 package com.github.krix38.flowly.model;
 
+import javax.xml.bind.annotation.XmlTransient;
+
 /**
  * Created by krix on 29.09.2017.
  */
+
+@XmlTransient
 public class FailureInformation {
 
     private Throwable exception;
+
+    public FailureInformation() {
+    }
 
     public FailureInformation(Throwable exception) {
         this.exception = exception;
